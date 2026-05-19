5943e0762c4458b4578582a4539557692bab196a
iko16-haekal <ikobagas69@gmail.com>
2026-05-19 22:54:45 +0700

    fix

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <h1>Vite + React mantap</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
-          cot {count}
+          ct {count}
         </button>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
