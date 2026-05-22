b4e2d452d0b47b730fa3a5a76b8ec3f64aeaac6a
iko16-haekal <ikobagas69@gmail.com>
2026-05-22 14:25:15 +0700

    fix: something

--- src/App.jsx
+++ src/App.jsx
@@ -15,7 +15,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React manta</h1>
+      <h1>Vite + React mana</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           {count}
